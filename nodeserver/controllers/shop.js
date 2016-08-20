b4eaa8c65a944271733697445ee1164723b616b0
sumanbh <sumanbh7@gmail.com>
2016-08-20 02:56:16 -0600

    filtering tweaks and added more options

--- nodeserver/controllers/shop.js
+++ nodeserver/controllers/shop.js
@@ -4,16 +4,12 @@ var db = app.get('db');
 module.exports = {
     getProducts: (req, res) => {
         const offset = (parseInt(req.params.page) - 1) * 24;
-        const limit = parseInt(req.params.page) * 24;
+        const limit = 24;
         var brand = req.query['brand'];
-        if (brand === 'undefined') brand = null;
-        db.get_all_products([brand], (err, products) => {
-            //get all of them, which will be response[0]
-            //push response[0].length onto response
-            //splice
-            //respond
-            // res.json(products);
+        console.log(brand);
 
+        if (brand === '') brand = null;
+        db.get_all_products([brand], (err, products) => {
             res.json({
                 total: products.length, 
                 data: products.splice(offset, limit)
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import { PaginatePipe, PaginationService, PaginationControlsCmp, IPaginationInstance } from 'ng2-pagination';
+import 'underscore';
 
 @Component({
     selector: 'shop',
@@ -16,24 +17,42 @@ import { PaginatePipe, PaginationService, PaginationControlsCmp, IPaginationInst
 
 export class ShopComponent implements OnInit {
     private _data: Observable<string[]>;
+    private _itemsPerPage: number = 24;
     private _page: number = 1;
-    public _brand: string = 'Asus';
     private _total: number;
-
+    private _brand: string= '';
+    
+    // public queryCollector: Array<string> = ['initial'];
 
     constructor(private http: Http) {
     }
 
     ngOnInit() {
-        this.getPage(1);
+        this.getPage(1, '');
     }
 
-    getPage(page: number) {
-        this._data = this.http.get(`/api/products/${page}?brand=${this._brand}&ram=8`)
+    public queryCollector = [];
+
+
+    getPage(page: number, _queryParam: string) {
+        if (this.queryCollector.length >= 1 && this.queryCollector.indexOf(_queryParam) !== -1) {
+            this.queryCollector = _.without(this.queryCollector, _queryParam);
+        }
+        else if (_queryParam.length > 0 && this.queryCollector.length === 0) this.queryCollector.push(_queryParam);
+        
+        console.log(this.queryCollector);
+
+        this._brand = this.queryCollector.join('');
+        console.log(this._brand);
+
+
+        
+        this._data = this.http.get(`/api/products/${page}?brand=${this._brand}`)
             .do((res: any) => {
                 this._page = page;
                 this._total = res.json().total;
             })
             .map((res: Response) => res.json().data);
     }
+
 }
