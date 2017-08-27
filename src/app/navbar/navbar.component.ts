5c77ce32d9a1bd5b8a3963d30cc9f2e333e1c40c
sumanbh <sumanbh7@gmail.com>
2017-08-27 15:08:08 -0600

    remove unused router

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Subscription } from 'rxjs/Subscription';
 import { NavService } from '../shared/nav.service';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nav-bar',
@@ -25,7 +24,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
     constructor(
         private userService: UserService,
         private navService: NavService,
-        private router: Router,
     ) {
     }
 
