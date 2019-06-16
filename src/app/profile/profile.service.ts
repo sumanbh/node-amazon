3114aa8c33c65928e76ce51c54610bf8f28dc3fd
Suman Bhattarai <sumanbh7@gmail.com>
2019-06-16 13:48:35 -0600

    remove unused rxjs map import

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
