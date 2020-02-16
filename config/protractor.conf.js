213fd387e3207b5c4b2416b8c5d7c54e5647c008
sumanbh <sumanbh7@gmail.com>
2020-02-16 11:03:21 -0700

    update port and fix google auth

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     browserName: 'chrome',
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://localhost:3000/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
